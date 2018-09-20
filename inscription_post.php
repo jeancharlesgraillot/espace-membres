3b530b496a06a9e937388927e6e7fc5803c45f07
jeancharlesgraillot <graillot.jc@gmail.com>
2018-09-20 16:30:13 +0200

    check pseudo

--- inscription_post.php
+++ inscription_post.php
@@ -10,32 +10,44 @@ $email = htmlspecialchars($_POST['email']);
 $pass_hache = password_hash($_POST['pass'], PASSWORD_DEFAULT);
 
 
-if (isset($pseudo) AND !empty($pseudo) AND isset($pass) AND !empty($pass) AND isset($pass_confirmation) AND !empty($pass_confirmation)
-AND isset($email) AND !empty($email) AND preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $email) AND ($pass == $pass_confirmation)){
+$reponse = $bdd->prepare('SELECT pseudo FROM membres WHERE pseudo = :pseudo');
+$reponse->execute(array(
+  'pseudo' => $pseudo
+));
 
-  $req = $bdd->prepare('INSERT INTO membres (pseudo, pass, email, date_inscription) VALUES(:pseudo, :pass, :email, CURDATE())');
-  $req->execute(array(
-      'pseudo' => $pseudo,
-      'pass' => $pass_hache,
-      'email' => $email,
-      ));
+$donnees = $reponse->fetch();
 
-  // Insertion of  product informations in database
-  echo "Merci pour votre inscription, veuillez vous connecter";
-  header('refresh:2;url=index.php');
+if ($donnees['pseudo'] == $pseudo) {
+  echo "Veuillez choisir un autre pseudo";
+  header('refresh:2;url=inscription.php');
+}else {
+  if (isset($pseudo) AND !empty($pseudo) AND isset($pass) AND !empty($pass) AND isset($pass_confirmation) AND !empty($pass_confirmation)
+  AND isset($email) AND !empty($email) AND preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $email) AND ($pass == $pass_confirmation)){
 
-}
-elseif ( empty($pseudo) OR empty($pass) OR empty($pass_confirmation) OR empty($email) ) {
-  echo 'Merci de remplir tous les champs !.';
-  header('refresh:2;url=inscription.php');
-}
-elseif ($pass != $pass_confirmation) {
-  echo "Resaisissez le(s) mots de passe !";
-  header('refresh:2;url=inscription.php');
-}
-elseif (!preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $email)) {
-  echo "L'email est incorrect !";
-  header('refresh:2;url=inscription.php');
+    $req = $bdd->prepare('INSERT INTO membres (pseudo, pass, email, date_inscription) VALUES(:pseudo, :pass, :email, CURDATE())');
+    $req->execute(array(
+        'pseudo' => $pseudo,
+        'pass' => $pass_hache,
+        'email' => $email,
+        ));
+
+    // Insertion of  product informations in database
+    echo "Merci pour votre inscription, veuillez vous connecter";
+    header('refresh:2;url=index.php');
+
+  }
+  elseif ( empty($pseudo) OR empty($pass) OR empty($pass_confirmation) OR empty($email)) {
+    echo 'Merci de remplir tous les champs !.';
+    header('refresh:2;url=inscription.php');
+  }
+  elseif ($pass != $pass_confirmation) {
+    echo "Resaisissez le(s) mots de passe !";
+    header('refresh:2;url=inscription.php');
+  }
+  elseif (!preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $email)) {
+    echo "L'email est incorrect !";
+    header('refresh:2;url=inscription.php');
+  }
 }
 
 ?>
