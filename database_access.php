fff4c241216dcb0a44f99bb52c4592e5fa3e92ea
jeancharlesgraillot <graillot.jc@gmail.com>
2018-09-25 09:35:56 +0200

    password retired

--- database_access.php
+++ database_access.php
@@ -2,7 +2,7 @@
 
 try
 {
-  $bdd = new PDO('mysql:host=localhost;dbname=espace_membres;charset=utf8', 'root', 'Strawberry591peaches', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+  $bdd = new PDO('mysql:host=localhost;dbname=espace_membres;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 }
 catch(Exception $e)
 {
